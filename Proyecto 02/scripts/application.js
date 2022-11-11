cf0b6bc554d394aa7fedbb10aef0426043d6b144
eauria12 <eauria@espol.edu.ec>
2022-11-11 08:38:21 -0500

    Petición realizada

--- Proyecto 02/scripts/application.js
+++ Proyecto 02/scripts/application.js
@@ -1,19 +1,29 @@
 
 window.onload = function () {
   
-  document.getElementById('cargar').addEventListener('click', () => {
-
-    let url = "https://random-d.uk/api/random"
+    let url = "https://api.jikan.moe/v4/anime"
 
     fetch(url)
       .then(response => response.json())
       .then(obj => {
-        document.getElementById('respuesta').innerHTML = JSON.stringify(obj)
+
+        let data = obj['data']
+        for(let i=0; i < data.length; i++){
+          let name = data[i].title
+          let episodes = data[i].episodes
+          let duration = data[i].duration
+          let status = data[i].status
+          let rating = data[i].rating
+          
+          let fila="<tr><td>"+name+"</td><td>"+episodes+"</td><td>"+duration+"</td><td>"+status+"</td><td>"+rating+"</td></tr>";
+
+          let body = document.createElement("tr");
+          body.innerHTML = fila;
+          document.getElementById("bodyTable").appendChild(body);
+        }
+
       })
       .catch(error => {
         document.getElementById('respuesta').innerHTML = JSON.stringify(error.message)
       })
-
-  })
-
 }
