83605312fef20e1308181f2ae864f4447a2657b1
eauria12 <eauria@espol.edu.ec>
2023-01-07 17:51:30 -0500

    Recurso

--- Proyecto 03/countries/src/app/service/resources.service.ts
+++ Proyecto 03/countries/src/app/service/resources.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResourcesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getData<T>(url: string) {
+    url = "https://restcountries.com/v3.1/all"
+    return this.http.get<T[]>(url)
+  }
 }
