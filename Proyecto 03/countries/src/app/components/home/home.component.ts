22572a59994fbf4f15a68fa70ae7b0982fc06597
eauria12 <eauria@espol.edu.ec>
2023-01-07 17:59:40 -0500

    Home Component

--- Proyecto 03/countries/src/app/components/home/home.component.ts
+++ Proyecto 03/countries/src/app/components/home/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { ResourcesService } from '../../service/resources.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  constructor(private resourcesService: ResourcesService) {
+  }
+
+  ngOnInit() {
+    this.resourcesService.getData<any[]>("").subscribe(response => {
+      
+      let countries= localStorage.getItem("countries");
+      if(!countries) {
+        localStorage.setItem("countries", JSON.stringify(response));
+      }
+
+    })
+  }
+
 
 }
