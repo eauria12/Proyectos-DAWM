b3188fd711fe84c62014af705d019f3e51b975c3
eauria12 <eauria@espol.edu.ec>
2023-01-31 03:21:48 -0500

    Products Table

--- Proyecto 04/admin/routes/index.js
+++ Proyecto 04/admin/routes/index.js
@@ -4,12 +4,12 @@ const axios = require('axios');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/customers', async function(req, res, next) {
-  
+router.get('/customers', async function (req, res, next) {
+
   const URL = 'http://localhost:4444/customers/findAll/json'
   const config = {
     proxy: {
@@ -18,23 +18,23 @@ router.get('/customers', async function(req, res, next) {
     }
   }
   const response = await axios.get(URL, config)
-  res.render('customers', { title: 'Customers', customers: response.data});
+  res.render('customers', { title: 'Customers', customers: response.data });
 
 });
 
 //API NO RELACIONAL
-router.get('/orders/:customerNumber', async function(req, res, next) {
+router.get('/orders/:customerNumber', async function (req, res, next) {
 
   const URL = 'https://api-sales-e096a-default-rtdb.firebaseio.com/collection.json?orderBy=%22customerNumber%22&equalTo=' + req.params.customerNumber
-  
+
   axios
     .get(URL)
     .then((response) => {
 
       let array = Object.values(response.data)
-      array = array.filter(order => order["status"]=="Shipped")
+      array = array.filter(order => order["status"] == "Shipped")
 
-      res.render('orders', { title: 'Orders', orders: array});
+      res.render('orders', { title: 'Orders', orders: array });
 
     })
     .catch((err) => {
@@ -43,19 +43,19 @@ router.get('/orders/:customerNumber', async function(req, res, next) {
 
 });
 
-router.get('/products', async function(req, res, next) {
+router.get('/products', async function (req, res, next) {
 
   const URL = 'https://api-sales-e096a-default-rtdb.firebaseio.com/collection.json'
-  
+
   axios
     .get(URL)
     .then((response) => {
 
       let array = Object.values(response.data)
+      let hash = {}
+      array = array.filter(p => hash[p.productCode] ? false : hash[p.productCode] = true);
 
-      
-
-      res.render('products', { title: 'Products', products: array});
+      res.render('products', { title: 'Products', products: array });
 
     })
     .catch((err) => {
@@ -64,6 +64,25 @@ router.get('/products', async function(req, res, next) {
 
 });
 
+/*router.post('/products', async function (req, res, next) {
 
+  const URL = 'https://api-sales-e096a-default-rtdb.firebaseio.com/collection.json'
+
+  axios
+    .get(URL)
+    .then((response) => {
+
+      let array = Object.values(response.data)
+      let hash = {}
+      array = array.filter(p => hash[p.productCode] ? false : hash[p.productCode] = true);
+
+      res.render('products', { title: 'Products', products: array });
+
+    })
+    .catch((err) => {
+      res.status(404).send(err);
+    });
+
+});*/
 
 module.exports = router;
