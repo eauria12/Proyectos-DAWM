1f20e321c313d76ecd010bd1b97913fe5acc3d5d
eauria12 <eauria@espol.edu.ec>
2023-01-31 01:54:27 -0500

    Orders Table

--- Proyecto 04/admin/routes/index.js
+++ Proyecto 04/admin/routes/index.js
@@ -18,7 +18,49 @@ router.get('/customers', async function(req, res, next) {
     }
   }
   const response = await axios.get(URL, config)
-  res.render('customers', { title: 'Customers', customers: response.data });
+  res.render('customers', { title: 'Customers', customers: response.data});
+
+});
+
+//API NO RELACIONAL
+router.get('/orders/:customerNumber', async function(req, res, next) {
+
+  const URL = 'https://api-sales-e096a-default-rtdb.firebaseio.com/collection.json?orderBy=%22customerNumber%22&equalTo=' + req.params.customerNumber
+  
+  axios
+    .get(URL)
+    .then((response) => {
+
+      let array = Object.values(response.data)
+      array = array.filter(order => order["status"]=="Shipped")
+
+      res.render('orders', { title: 'Orders', orders: array});
+
+    })
+    .catch((err) => {
+      res.status(404).send(err);
+    });
+
+});
+
+router.get('/products', async function(req, res, next) {
+
+  const URL = 'https://api-sales-e096a-default-rtdb.firebaseio.com/collection.json'
+  
+  axios
+    .get(URL)
+    .then((response) => {
+
+      let array = Object.values(response.data)
+
+      
+
+      res.render('products', { title: 'Products', products: array});
+
+    })
+    .catch((err) => {
+      res.status(404).send(err);
+    });
 
 });
 
